0fd879e6137c0d11549ebc9de463772a6ad4c5ea
Mannar Hielal <adminwithu@gmail.com>
2020-06-03 09:28:08 +0200

    Implement Serializers Validation methods

--- backend/news/api/urls.py
+++ backend/news/api/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 
-from news.api.views import (article_list_create_api_view,
-                            article_detail_api_view,
-                            ArticleListCreateAPIView,
+from news.api.views import (ArticleListCreateAPIView,
                             ArticleDetailAPIView)
 
 urlpatterns = [
